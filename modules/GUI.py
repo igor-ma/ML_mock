9232e478cd7da52ad99ff8eada99e00e1bd741c2
igor-ma <igorma.academico@gmail.com>
2024-07-16 11:24:24 -0300

    updating GUI to worki with pipeline class;

--- modules/GUI.py
+++ modules/GUI.py
@@ -4,6 +4,7 @@ import streamlit as st
 from datetime import datetime
 from modules.database import Database
 from modules.data_utils import DataUtils
+from modules.pipeline import Pipeline
 
 
 class GUI:
@@ -12,6 +13,7 @@ class GUI:
     def __init__(self, db: Database) -> None:
         '''Start the main GUI using Streamlit'''
         self.db = db
+        self.pipeline = Pipeline(db)
 
         st.title('Case - Fine-tuning e Deploy de LLMs')
         tabs = ['Registro de Datasets', 'Fine-tuning', 'Deploy', 'Swagger', 'Dashboard']
@@ -40,24 +42,11 @@ class GUI:
         language = st.text_input('Idioma')
 
         if st.button('Salvar'):
-            #get data split paths to save on database
-            train_path, val_path, test_path = DataUtils().processDataset(
-                txt_path.name)
-            
-            #prepare data to save on database
-            data = {
-                'train_path': train_path,
-                'val_path': val_path,
-                'test_path': test_path,
-                'source': source,
-                'date': date,
-                'language': language,
-                'name': name
-            }
-
-            #save
-            self.db.insertDatasets(data)
-            st.success('Dados salvos com sucesso.')
+            try:
+                self.pipeline.registerDataset(txt_path.name, source, date, language, name)
+                st.success('Dados salvos com sucesso.')
+            except:
+                st.error('Erro de inserção.')
 
     def fine_tuning(self):
         '''Configure tab for running fine-tunings'''
@@ -65,21 +54,25 @@ class GUI:
         #list available datasets to fine-tune on
         self.db.cursor.execute('SELECT id, name FROM Datasets')
         datasets = self.db.cursor.fetchall()
-        dataset_options = [f"{dataset[0]}, {dataset[1]}" for dataset in datasets]
+        datasets_list = [f"{dataset[0]}, {dataset[1]}" for dataset in datasets]
 
         #create tab elements
         st.subheader('Fine-tuning')
         model = st.text_input('Modelo')
-        datasets = st.selectbox('Datasets disponíveis', dataset_options)
-        option = st.radio('Opção', ['Clássico', 'LoRA'])
+        ds_option = st.selectbox('Datasets disponíveis', datasets_list)
+        ft_option = st.radio('Opção', ['Clássico', 'LoRA'])
         
         #enable LoRA specific parameters
-        if option == 'LoRA':
+        if ft_option == 'LoRA':
             ranking = st.select_slider('Ranking', options=[2, 4, 8, 16, 32, 64, 128, 256])
+        else:
+            ranking = None
         
         learning_rate = st.number_input('Learning Rate', min_value=0.0, max_value=1.0, step=0.01)
         if st.button('Fine-tune'):
-            st.success('Fine-tuning em andamento.')
+            with st.spinner('Fine-tuning em andamento...'):
+                self.pipeline.fineTuneModel(model, ds_option, ft_option, ranking, learning_rate)
+                st.success('Fine-tuning executado com sucesso.')
 
     def deploy(self):
         '''Configure tab for deploying models'''
