be757147579dca7b5aa03b8dec5cf279b2073b69
igor-ma <igorma.academico@gmail.com>
2024-07-16 13:04:32 -0300

    checkpoint: adding fine-tuning pipeline;

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -2,8 +2,10 @@
 
 from modules.data_utils import DataUtils
 from modules.database import Database
-import streamlit as st
 import datetime
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, Seq2SeqTrainer, Seq2SeqTrainingArguments
+from datasets import load_dataset
 
 
 class Pipeline:
@@ -30,8 +32,58 @@ class Pipeline:
         #save
         self.db.insertDatasets(data)
 
-    def fineTuneModel(self, model, ds_option, ft_option, ranking, learning_rate):
-        pass
+
+    def _train(self, model_name: str, learning_rate: float, train_dataset, val_dataset):
+        device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+
+        #set training parameters
+        training_args = Seq2SeqTrainingArguments(
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            learning_rate=learning_rate,
+            evaluation_strategy="epoch",
+            logging_steps=10,
+            save_steps=10,
+            eval_steps=10,
+            output_dir="./results",
+            logging_dir="./logs",
+            overwrite_output_dir=True,
+            save_total_limit=3,
+            num_train_epochs=1
+        )
+
+        #set trainer
+        trainer = Seq2SeqTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=val_dataset,
+            tokenizer=tokenizer
+        )
+
+        #fine-tune
+        trainer.train()
+
+        #save final model
+        trainer.save_model()
+
+        return trainer
+      
+    def _eval(self, trainer, test_dataset):
+        return trainer.evaluate(test_dataset=test_dataset)
+
+    def fineTuneModel(self, model_name: str, ds_option: str, ft_option: str, ranking: int, learning_rate: float):
+        train_dataset = load_dataset('text', data_files="data/2c1d6990-c264-4442-bf9c-3776f7d3bb4a/train.txt")
+        val_dataset = load_dataset('text', data_files="data/2c1d6990-c264-4442-bf9c-3776f7d3bb4a/val.txt")
+        test_dataset = load_dataset('text', data_files="data/2c1d6990-c264-4442-bf9c-3776f7d3bb4a/test.txt")
+
+        #fine-tune 
+        trainer = self._train(model_name, learning_rate, train_dataset, val_dataset)
+
+        #evaluate final model
+        results = self._eval(trainer, test_dataset)
 
     def deployModel(self):
         pass
