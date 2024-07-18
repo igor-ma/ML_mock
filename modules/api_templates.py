f39c1bcc56c316f8e37fd6b69b28dd1c3483e724
igor-ma <igorma.academico@gmail.com>
2024-07-18 13:14:53 -0300

    making APIs async (useful for LLMs/Gen AI);

--- modules/api_templates.py
+++ modules/api_templates.py
@@ -19,25 +19,59 @@ import torch
 
 app = FastAPI()
 
-#load model
-tokenizer = AutoTokenizer.from_pretrained('{}')
-model = AutoModelForSequenceClassification.from_pretrained('{}')
+tokenizer = None
+model = None
+
+async def loadTokenizer():
+    '''Async. function to load tokenizer'''
+
+    global tokenizer
+    tokenizer = AutoTokenizer.from_pretrained('{}')
+
+async def loadModel():
+    '''Async. function to load model'''
+
+    global model
+    model = AutoModelForSequenceClassification.from_pretrained('{}')
+
+async def runTokenization(text):
+    '''Async. function to tokenize'''
+
+    global tokenizer
+    await loadTokenizer() #ensure tokenizer is loaded
+
+    #tokenize
+    inputs = tokenizer(text, return_tensors="pt")
+
+    return inputs
+
+async def runInference(inputs):
+    '''Async. function to infer'''
+
+    global model
+    await loadModel() #ensure model is loaded
+
+    #infer
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+    
+    return logits
 
 #request data type
 class PredictionRequest(BaseModel):
     text: str
 
 @app.post('/predict')
-def predict(request_data: PredictionRequest):
+async def predict(request_data: PredictionRequest):
+    #get request text
     text = request_data.text
     
     #tokenize text
-    inputs = tokenizer(text, return_tensors="pt")
+    inputs = await runTokenization(text)
 
     #predict
-    with torch.no_grad():
-        outputs = model(**inputs)
-        logits = outputs.logits
+    logits = await runInference(inputs)
     
     #get predicted class from logits
     predicted_class = torch.argmax(logits, dim=1).item()
